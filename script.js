ec0b39247268a00fa6f84dcadde0543f30bf41c3
tobiasabasdev <tobiasabasdev@gmail.com>
2025-08-28 14:16:39 -0300

    Sidebar animation,Router,Contactform Responsive

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function showSidebar() {
-const sidebar = document.querySelector('.sidebar');
-sidebar.style.display = "flex";
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('hide');
+    sidebar.classList.add('show');
 }
 
-function hideSidebar(){
-const sidebar = document.querySelector('.sidebar');
-sidebar.style.display = "none";
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.remove('show');
+    sidebar.classList.add('hide');
+    sidebar.addEventListener('animationend', function handler() {
+        if (sidebar.classList.contains('hide')) {
+            sidebar.style.display = "none";
+        }
+        sidebar.removeEventListener('animationend', handler);
+    });
 }
