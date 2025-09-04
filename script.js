1afe050d6a1462b8e4e373ff5a8b5590e1eeb94c
tobiasabasdev <tobiasabasdev@gmail.com>
2025-09-04 09:40:28 -0300

    Cambios js nueva card a traves del dom

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ function hideSidebar() {
         sidebar.removeEventListener('animationend', handler);
     });
 }
+
+const listaProductos = document.getElementById('listaproductos');
+function agregarProducto(nombre,descripcion,imagen,precio) { 
+const card = document.createElement('article');
+card.classList.add('card');
+
+card.innerHTML = `<div class=  "card-content">
+<h3>${nombre}</h3>
+<p>${descripcion}</p>
+<img src="${imagen}" alt="${imagen}">
+<p class= "price" > $ARS ${precio} </p>
+</div>
+`;
+
+listaProductos.append(card);
+}
+
+agregarProducto(
+  "Joystick",
+  "Joystick gamer con vibración",
+  "img/joystick.jpg",
+  12000,
+
+);
+
+
+console.log(listaProductos.innerHTML);
